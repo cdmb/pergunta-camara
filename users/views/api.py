080d32f93bb23603843cb56748eec11c1a6c3b14
Alexandre <0x41e@protonmail.ch>
2016-11-11 14:30:15 -0200

    Add routes, fix deps and try swagger

--- users/views/api.py
+++ users/views/api.py
@@ -1,6 +1,22 @@
-from pyramid.view import view_config
+from pyramid.view import view_config, view_defaults
 
 
-@view_config(route_name='user_hello_world', renderer='json')
-def user_hello_world(request):
-    return {'hello': 'world'}
+@view_defaults(renderer='json')
+class Users:
+
+    def __init__(self, request):
+        self.request = request
+        self.view_name = 'Users'
+
+
+    @view_config(route_name='desc')
+    def descript_users(self):
+        return {'msg': 'Olá'}
+
+    @view_config(route_name='list_users', request_method='GET')
+    def list_all_users(self):
+        return [{'id': '1', 'username': 'foo'}]
+
+    @view_config(route_name='create_user', request_method='POST')
+    def create_user(self):
+        return {2: 3}
--- users/views/routes.py
+++ users/views/routes.py
@@ -1,2 +1,4 @@
 def includeme(config):
-    config.add_route('user_hello_world', '/users/')
+    config.add_route('desc', '/users')
+    config.add_route('list_users', '/users/list')
+    config.add_route('create_user', '/users/create')
