80009958162749a48b4034e64d84cec4146339bd
Alexandre <0x41e@protonmail.ch>
2016-11-26 12:25:12 -0200

    Create the model 'Author'

--- communications/models.py
+++ communications/models.py
@@ -4,6 +4,19 @@ from sqlalchemy.orm import backref, relationship
 from pergunta_camara.utils.models import Base, CreatedUpdatedMixin, get_uuid
 
 
+class Author(Base, CreatedUpdatedMixin):
+
+    __tablename__ = 'author'
+
+    id = sa.Column(sa.Integer, primary_key=True)
+    uuid = sa.Column(
+        sa.String(32), nullable=False, unique=True, index=True,
+        default=get_uuid
+    )
+
+    name = sa.Column(sa.String(50), nullable=False, unique=False)
+
+
 class Communication(Base, CreatedUpdatedMixin):
 
     __tablename__ = 'communication'
@@ -33,10 +46,10 @@ class Message(Base, CreatedUpdatedMixin):
         foreign_keys=[communication_id]
     )
 
-    profile_id = sa.Column(sa.Integer, sa.ForeignKey('profile.id'), index=True)
-    profile = relationship(
-        'Profile', backref=backref('messages', lazy='dynamic'),
-        foreign_keys=[profile_id]
+    author_id = sa.Column(sa.Integer, sa.ForeignKey('author.id'), index=True)
+    author = relationship(
+        'Author', backref=backref('messages', lazy='dynamic'),
+        foreign_keys=[author_id]
     )
 
     state = sa.Column(sa.String(50), nullable=False, unique=False, index=True)
