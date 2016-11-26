13cccdd900981e65d30f470cac2632b6a3888fa7
Alexandre <0x41e@protonmail.ch>
2016-11-26 12:59:12 -0200

    Delete profiles module

--- profiles/__init__.py
+++
@@ -1,7 +0,0 @@
-from .views.routes import includeme as views_include
-
-
-def includeme(config):
-
-    config.include(views_include)
-    config.scan()
--- profiles/views/routes.py
+++
@@ -1,7 +0,0 @@
-from .resources import ProfilesResource
-
-
-def includeme(config):
-
-    # Profiles service
-    config.add_route('profiles', '/profiles', factory=ProfilesResource)
